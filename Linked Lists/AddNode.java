77643ad36f6ee46006ee39dd59724ce5a34752b2
aditi157 <aditichauhan0175@gmail.com>
2025-12-10 09:45:22 +0530

    Add a connection from the tail of the linked list to the node at the given position, forming a cycle

--- Linked Lists/AddNode.java
+++ Linked Lists/AddNode.java
@@ -8,6 +8,20 @@ class Node{
     }
 }
 public class AddNode {
+    static Node connect(Node head, int pos){
+        Node tail=head;
+        while(tail.next!=null){
+            tail=tail.next;
+        }
+        int index=0;
+        Node target=head;
+        while(index<pos){
+            target=target.next;
+            index++;
+        }
+        tail.next=target;
+        return tail;
+    }
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number of nodes: ");
@@ -26,17 +40,12 @@ public class AddNode {
                 temp=temp.next;
             }
         }
-        Node tail=temp;
         System.out.print("Enter postion to which the last node should connect to (head=pos 0): ");
         int pos=sc.nextInt();
-        int index=0;
-        Node target=head;
-        while(index<pos){
-            target=target.next;
-            index++;
-        }
-        tail.next=target;
+        Node tail=connect(head, pos);
         System.out.print("Nodes connected. The tail ("+tail.data+") connects to the node with data: "+tail.next.data);
         sc.close();
     }
 }
+
+
