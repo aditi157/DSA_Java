96bc936a9b5ffa75ef0398be57416a13d07e4c9c
aditi157 <aditichauhan0175@gmail.com>
2025-12-10 09:30:44 +0530

    Connect the tail node to the node at given position.
    Implement linked list node addition with user input for connections.

--- Linked Lists/AddNode.java
+++ Linked Lists/AddNode.java
@@ -1,1 +1,42 @@
-
+import java.util.*;
+class Node{
+    int data;
+    Node next;
+    Node(int n){
+        this.data=n;
+        this.next=null;
+    }
+}
+public class AddNode {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of nodes: ");
+        int n=sc.nextInt();
+        Node head=null;
+        Node temp=null;
+        System.out.print("Enter data for all nodes: ");
+        for(int i=0;i<n;i++){
+            int x=sc.nextInt();
+            Node newNode=new Node(x);
+            if(head==null){
+                head=newNode;
+                temp=newNode;
+            }else{
+                temp.next=newNode;
+                temp=temp.next;
+            }
+        }
+        Node tail=temp;
+        System.out.print("Enter postion to which the last node should connect to (head=pos 0): ");
+        int pos=sc.nextInt();
+        int index=0;
+        Node target=head;
+        while(index<pos){
+            target=target.next;
+            index++;
+        }
+        tail.next=target;
+        System.out.print("Nodes connected. The tail ("+tail.data+") connects to the node with data: "+tail.next.data);
+        sc.close();
+    }
+}
